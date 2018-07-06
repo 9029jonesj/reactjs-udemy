7e9f2267f24cccc0fa50492d244af11551f0ab4f
Jerrell Jones <jerrell.l.jones@gmail.com>
2018-07-06 00:30:53 -0400

    redux/reducers

--- expensify/src/playground/redux-101.js
+++ expensify/src/playground/redux-101.js
@@ -2,62 +2,77 @@ import {
     createStore
 } from 'redux';
 
-const store = createStore((state = {
+const incrementCount = ({
+    incrementBy = 1
+} = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({
+    decrementBy = 1
+} = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count } = {}) => ({
+    type: 'SET',
+    count
+});
+
+const reset = () => ({
+    type: 'RESET',
+    count: 0
+});
+// Reducers
+// 1. Reducers are pure functions (ie. Output only depends on input. Does not interact with anything outside of its scope)
+// 2. Never change state or action.
+const countReducer = (state = {
     count: 0
 }, action) => {
-    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
-    const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
     switch (action.type) {
         case 'INCREMENT':
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'RESET':
             return {
-                count: 0
+                count: action.count
             };
         case 'SET':
             return {
                 count: action.count
-            }; 
+            };
         default:
             return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 });
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
+store.dispatch(incrementCount({
+    incrementBy: 7
+}));
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
+
+store.dispatch(reset());
 
 // unsubscribe();
 
-store.dispatch({
-    type: 'RESET'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-    type: 'DECREMENT'
-});
-
-store.dispatch({
-    type: 'DECREMENT',
+store.dispatch(decrementCount({
     decrementBy: 10
-});
+}));
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+store.dispatch(setCount({ count: 999 }));
---
+++ expensify/src/playground/redux-expensify.js
@@ -0,0 +1,20 @@
+import {
+    createStore,
+    combineReducers
+} from 'redux';
+
+const demoState = {
+    expenses: [{
+        id: 'testId',
+        descripton: 'January Rent',
+        note: 'This was the final payment',
+        amount: 54500,
+        createdAt: 0
+    }],
+    filters: {
+        text: 'rent',
+        sortBy: 'amount', // date or amount
+        startDate: undefined,
+        endDate: undefined
+    }
+};
