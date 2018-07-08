39c1b65f954191396cb559004f9a6cdeef03e646
Jerrell Jones <jerrell.l.jones@gmail.com>
2018-07-08 09:33:05 -0400

    Expensify updates

--- expensify/src/playground/redux-expensify.js
+++ expensify/src/playground/redux-expensify.js
@@ -3,6 +3,174 @@ import {
     combineReducers
 } from 'redux';
 
+import uuid from 'uuid';
+
+// Actions
+const addExpense = ({
+    descripton = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+} = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        descripton,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+const removeExpense = ({
+    id
+} = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT',
+    sortBy: 'amount'
+});
+
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE',
+    sortBy: 'date'
+});
+
+const setStartDate = (startDate = undefined) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
+const setEndDate = (endDate = undefined) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
+
+// Expenses Reducer
+
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ]
+        case 'REMOVE_EXPENSE':
+            return state.filter(({
+                    id
+                }) =>
+                id !== action.id
+            );
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                }
+                return expense;
+            })
+        default:
+            return state;
+    }
+};
+
+// Filters Reducer
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: action.sortBy
+            };
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: action.sortBy
+            };
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            };
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            };
+        default:
+            return state;
+    }
+};
+
+// Store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+console.log(store.getState());
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({
+    descripton: 'Rent',
+    amount: 10000
+}));
+const expenseTwo = store.dispatch(addExpense({
+    descripton: 'Coffee',
+    amount: 300
+}));
+
+store.dispatch(removeExpense({
+    id: expenseOne.expense.id
+}));
+
+store.dispatch(editExpense(expenseTwo.expense.id, {
+    amount: 500
+}));
+
+store.dispatch(setTextFilter('Rent'));
+store.dispatch(setTextFilter());
+
+store.dispatch(sortByAmount());
+store.dispatch(sortByDate());
+
+store.dispatch(setStartDate('May-2018'));
+store.dispatch(setEndDate('August-2018'));
+
 const demoState = {
     expenses: [{
         id: 'testId',
@@ -18,3 +186,13 @@ const demoState = {
         endDate: undefined
     }
 };
+
+const user = {
+    name: 'Jerrell',
+    age: 26
+};
+
+console.log({
+    ...user,
+    city: 'Baltimore'
+});
